464da5f35612477312420446cb30512c75cce73b
hijakd <jharyd@gmail.com>
2021-03-06 18:33:41 +1100

    display use of flex

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.view1}/>
+      <View style={styles.view2}/>
     </View>
   );
 }
@@ -14,8 +14,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-});
+  view1:{
+    backgroundColor: '#293250',
+    flex:1,
+  },
+  view2:{
+    backgroundColor: '#ffd55a',
+    flex:1,
+  },
+})
