159933cc593ca43372654645689947956cd4b0c5
hijakd <jharyd@gmail.com>
2021-03-06 19:07:47 +1100

    wrapping and justifying content

--- App.js
+++ App.js
@@ -8,6 +8,9 @@ export default function App() {
       <View style={styles.view1}/>
       <View style={styles.view2}/>
       <View style={styles.view3}/> 
+      <View style={styles.view4}/>
+      <View style={styles.view5}/>
+      <View style={styles.view6}/> 
     </View>
   );
 }
@@ -17,10 +20,11 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection:"row",
     // justifyContent:'center', // justifies content along the "main axis"
-    justifyContent:'space-around', // justifies content along the "main axis" with 'padding'
-    alignItems:'center', // aligns items along the "cross axis"
-    
-
+    // justifyContent:'space-around', // justifies content along the "main axis" with 'padding' 
+    justifyContent:'space-evenly', // alternatively use "space-evenly" 
+    // alignItems:'center', // aligns items along the "cross axis"
+    alignContent:'center',
+    flexWrap:'wrap',
 
     // backgroundColor: '#fff',
     // alignItems: 'center',
@@ -29,19 +33,37 @@ const styles = StyleSheet.create({
   view1:{
     backgroundColor: '#293250',
     // flex:1,
-    height:100,
-    width:100,
+    height:110,
+    width:110,
   },
   view2:{
     backgroundColor: '#ffd55a',
     // flex:1,
-    height:100,
-    width:100,
+    height:110,
+    width:110,
   },
   view3:{
     backgroundColor: '#6dd47e',
     // flex:1,
-    height:100,
-    width:100,
+    height:110,
+    width:110,
+  },
+  view4:{
+    backgroundColor: '#293351',
+    // flex:1,
+    height:110,
+    width:110,
+  },
+  view5:{
+    backgroundColor: '#ffd65b',
+    // flex:1,
+    height:110,
+    width:110,
+  },
+  view6:{
+    backgroundColor: '#6dd57f',
+    // flex:1,
+    height:110,
+    width:110,
   },
 })
